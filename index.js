0b1194dea69cdfbedbeae818ed966490a46a5003
Lionblaze <59327609+LionblazeYT@users.noreply.github.com>
2019-12-28 23:45:47 -0800

    minor compression
    o p e r a t i o n   s p e e d

--- index.js
+++ index.js
@@ -53,12 +53,9 @@ app.parseResponse = function (responseBody, splitter) {
 
 //xss bad
 app.clean = function(text) {if (!text || typeof text != "string") return text; else return text.replace(/&/g, "&#38;").replace(/</g, "&#60;").replace(/>/g, "&#62;").replace(/=/g, "&#61;").replace(/"/g, "&#34;").replace(/'/g, "&#39;")}
-
 console.log("Site online!")
 
-
 // ASSETS
-
 let assets = ['css', 'assets', 'blocks', 'deatheffects', 'difficulty', 'gauntlets', 'gdicon', 'iconkitbuttons', 'levelstyle', 'objects']
 app.use('/css', express.static(__dirname + '/assets/css'));
 app.use('/assets', express.static(__dirname + '/assets', {maxAge: "7d"}));
@@ -71,21 +68,16 @@ app.use('/iconkitbuttons', express.static(__dirname + '/assets/iconkitbuttons', 
 app.use('/levelstyle', express.static(__dirname + '/assets/initial', {maxAge: "7d"}));
 app.use('/objects', express.static(__dirname + '/assets/objects', {maxAge: "7d"}));
 
-
 // POST REQUESTS
-
 app.post("/like", function(req, res) { app.run.like(app, req, res) })  
 app.post("/postComment", function(req, res) { app.run.postComment(app, req, res) })  
 app.post("/postProfileComment", function(req, res) { app.run.postProfileComment(app, req, res) })  
-
 app.post("/messages", async function(req, res) { app.run.getMessages(app, req, res, api) })
 app.post("/messages/:id", async function(req, res) { app.run.fetchMessage(app, req, res, api) })
 app.post("/deleteMessage", function(req, res) { app.run.deleteMessage(app, req, res) })  
 app.post("/sendMessage", function(req, res) { app.run.sendMessage(app, req, res) })  
 
-
 // HTML
-
 app.get("/", function(req, res) { res.sendFile(__dirname + "/html/home.html") })   
 app.get("/analyze/:id", async function(req, res) { res.sendFile(__dirname + "/html/analyze.html") })
 app.get("/api", function(req, res) { res.sendFile(__dirname + "/html/api.html") })
@@ -98,9 +90,7 @@ app.get("/mappacks", function(req, res) { res.sendFile(__dirname + "/html/mappac
 app.get("/search", function(req, res) { res.sendFile(__dirname + "/html/filters.html") })
 app.get("/search/:text", function(req, res) { res.sendFile(__dirname + "/html/search.html") })
 
-
 // API
-
 app.get("/api/analyze/:id", async function(req, res) { app.run.level(app, req, res, api, true) })
 app.get("/api/comments/:id", function(req, res) { app.run.comments(app, req, res, api) })
 app.get("/api/credits", function(req, res) { res.send(require('./misc/credits.json')) })
@@ -112,13 +102,10 @@ app.get("/api/profile/:id", function(req, res) { app.run.profile(app, req, res, 
 app.get("/api/search/:text", function(req, res) { app.run.search(app, req, res, api) })
 
 // API AND HTML
-   
 app.get("/profile/:id", function(req, res) { app.run.profile(app, req, res) })
 app.get("/:id", function(req, res) { app.run.level(app, req, res) }) 
- 
 
 // REDIRECTS
-
 app.get("/icon", function(req, res) { res.redirect('/iconkit') })
 app.get("/iconkit/:text", function(req, res) { res.redirect('/icon/' + req.params.text) })
 app.get("/leaderboards/:id", function(req, res) { res.redirect('/leaderboard/' + req.params.id) })
@@ -128,13 +115,10 @@ app.get("/c/:id", function(req, res) { res.redirect('/comments/' + req.params.id
 app.get("/u/:id", function(req, res) { res.redirect('/profile/' + req.params.id) })
 app.get("/p/:id", function(req, res) { res.redirect('/profile/' + req.params.id) })
 
-
 // MISC
-
 app.get("/assets/sizecheck.js", function(req, res) { res.sendFile(__dirname + "/misc/sizecheck.js") }) 
 app.get('/api/icons', function(req, res) { res.send(gdicons); });
 app.get("/icon/:text", function(req, res) { app.run.icon(app, req, res) })
-
 app.get('*', function(req, res) {
   if (req.path.startsWith('/api')) res.send('-1')
   if (assets.some(x => req.path.startsWith("/" + x))) res.send("Looks like this file doesn't exist ¯\\_(ツ)_/¯<br>You can check out all of the assets on <a target='_blank' href='https://github.com/GDColon/GDBrowser/tree/master/assets'>GitHub</a>")
