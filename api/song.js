0f42708e315597969ff7d698e45e91f541c2bd7d
GDColon <therealgdcolon@gmail.com>
2021-01-05 11:14:56 -0500

    wait i think rob hardcodes certain songs
    citation: future funk

--- api/song.js
+++ api/song.js
@@ -1,7 +1,5 @@
 const request = require('request')
 
-let newSong = 732000 // rough estimate of the point when the whitelisted song system was implemented. (around march 1st, 2017)
-
 module.exports = async (app, req, res) => {
 
     let info = {error: true, exists: false, artist: { name: "", scouted: false, whitelisted: false }, song: { name: "", externalUse: false, allowed: false } }
@@ -11,8 +9,10 @@ module.exports = async (app, req, res) => {
     let songID = req.params.song
     
     request.post(app.endpoint + 'testSong.php?songID=' + songID, req.gdParams(), async function(err, resp, body) {
-    
-        if (err || !body || body == '-1' || body.startsWith("<!")) return res.send(info)
+    if (err || !body || body == '-1' || body.startsWith("<!")) return res.send(info)
+
+    request.post(app.endpoint + 'getGJSongInfo.php' + songID, req.gdParams({songID: songID}), async function(err2, resp, songAllowed) {
+        if (err2 || !songAllowed || songAllowed < 0 || body.startsWith("<!")) return res.send(info)
 
         let artistInfo = body.split(/<\/?br>/)
         info.artist.name = artistInfo[0].split(": ")[1]
@@ -21,10 +21,11 @@ module.exports = async (app, req, res) => {
         info.artist.whitelisted = artistInfo[1].split("is NOT").length == 1
         info.song.name = artistInfo[4].split(": ")[1]
         info.song.externalUse = artistInfo[5].split("API NOT").length == 1
-        info.song.allowed = info.artist.scouted && info.song.externalUse && (+songID > newSong ? info.artist.whitelisted : true)
+        info.song.allowed = songAllowed.length > 15
 
         delete info.error
         res.send(info)
 
+    })
     })
 }
