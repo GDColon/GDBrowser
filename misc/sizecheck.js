2cc8f87810792177beb8dcddd5384968ff48cc7b
Gav Hern <47736824+GavHern@users.noreply.github.com>
2021-05-28 09:54:53 -0700

    Add additional accessibilty (#150)
    * Add additional accessibilty
    
    * Requested revisions for accessibility commit

--- misc/sizecheck.js
+++ misc/sizecheck.js
@@ -67,3 +67,24 @@ while ($(this).scrollTop() != 0) {
 $(document).ready(function() {
 	$(window).trigger('resize');
 });
+
+
+
+// Adds all necessary elements into the tab index (all buttons and links that aren't natively focusable)
+const inaccessibleLinkSelector = "*:not(a) > img.gdButton, .leaderboardTab, .gdcheckbox";
+
+document.querySelectorAll(inaccessibleLinkSelector).forEach(elem => {
+  elem.setAttribute('tabindex', 0);
+})
+
+document.getElementById('backButton')?.setAttribute('tabindex', 1); // Prioritize back button, first element to be focused
+
+
+// Event listener to run a .click() function if
+window.addEventListener("keydown", e => {
+  if(e.key !== 'Enter') return;
+
+  const active = document.activeElement;
+  const isUnsupportedLink = active.hasAttribute('tabindex'); // Only click on links that aren't already natively supported to prevent double clicking
+  if(isUnsupportedLink) active.click();
+})
