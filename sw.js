5a9b5c5fcf89b0dbdd97e8ba936928d96811eb8c
fgclue <schefferdaudtfelipe@gmail.com>
2022-06-17 23:46:36 -0300

    oh god....

---
+++ sw.js
@@ -0,0 +1,20 @@
+const staticDevCoffee = "gdbrowserv1"
+const assets = [
+    "/" // please someone make this list not only have the index
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
