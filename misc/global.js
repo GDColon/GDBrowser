add4af4432c33ad069b81b7865c0495b62eec7af
fgclue <schefferdaudtfelipe@gmail.com>
2022-07-02 10:18:23 -0300

    sadfsdfasdf

--- misc/global.js
+++ misc/global.js
@@ -8,6 +8,14 @@ $('body').append(`
 	</div>
 `)
 
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", function() {
+	navigator.serviceWorker
+	  .register("/serviceWorker.js")
+	  .then(res => console.log("service worker registered"))
+	  .catch(err => console.log("service worker not registered", err))
+	})
+	}
 
 $(window).resize(function () {
 	if (window.innerHeight > window.innerWidth - 75) { 
